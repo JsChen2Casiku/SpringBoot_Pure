8b28c4d131b91c5af3cc4f37705f53f9bd6e611c
JasonChen <JasonChenK@163.com>
2024-04-26 17:31:22 +0800

    init

---
+++ src/main/java/com/springboot/pure/SpringBootPureApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.pure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPureApplication.class, args);
+    }
+
+}
